a440580b232abb6d1c5e574fe04bb90fe39ffc30
xyderos <kostas@xyderos.com>
2023-03-19 09:02:52 +0100

    added documentation

--- src/synchronization/coordinate_based_matrix_sum/coordinate.h
+++ src/synchronization/coordinate_based_matrix_sum/coordinate.h
@@ -1,3 +1,5 @@
 #pragma once
 
+//utilize strips of the matrix in order to calculate sums of each worker
+// minimum and maximum values as well as their coordinates
 int coordinate_based_matrix_sum(void);
