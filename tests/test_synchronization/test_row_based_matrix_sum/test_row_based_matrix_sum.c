4bc47e75b7e5ba26965ed1811bef9a666092aa8c
xyderos <kostas@xyderos.com>
2023-03-19 09:07:02 +0100

    added test case for row based matrix sum

---
+++ tests/test_synchronization/test_row_based_matrix_sum/test_row_based_matrix_sum.c
@@ -0,0 +1,17 @@
+#include "test_row_based_matrix_sum.h"
+
+#include <check.h>
+
+Suite *row_suite(void) {
+  Suite *s = NULL;
+  TCase *tc_core = NULL;
+
+  s = suite_create("row");
+  tc_core = tcase_create("core");
+
+  tcase_add_test(tc_core, TEST_ROW_SHOULD_BE_OKAY);
+
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
