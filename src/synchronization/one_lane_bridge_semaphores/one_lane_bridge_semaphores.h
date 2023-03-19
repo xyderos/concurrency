eeaa86d0e139c3d94046d7d7f38cb1b3ceea025d
xyderos <kostas@xyderos.com>
2023-03-19 17:31:16 +0100

    added one lane bridge update

--- src/synchronization/one_lane_bridge_semaphores/one_lane_bridge_semaphores.h
+++ src/synchronization/one_lane_bridge_semaphores/one_lane_bridge_semaphores.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//cars from the same direction can cross the bridge at the same time
-// cars from the opposite direction cant
-// ensures order, cars leave on the same order they enter the bridge
+// cars from the same direction can cross the bridge at the same time
+//  cars from the opposite direction cant
+//  ensures order, cars leave on the same order they enter the bridge
 int one_lane_bridge(void);
