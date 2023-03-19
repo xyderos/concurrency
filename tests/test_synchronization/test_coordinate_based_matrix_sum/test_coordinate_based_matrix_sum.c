7d9ae9f678b8cbd4b2acc0ec69440ffcb0f00a7b
xyderos <kostas@xyderos.com>
2023-03-19 08:28:38 +0100

    added coordinate test

---
+++ tests/test_synchronization/test_coordinate_based_matrix_sum/test_coordinate_based_matrix_sum.c
@@ -0,0 +1,17 @@
+#include "test_coordinate_based_matrix_sum.h"
+
+#include <check.h>
+
+Suite *coordinate_suite(void) {
+  Suite *s = NULL;
+  TCase *tc_core = NULL;
+
+  s = suite_create("coordinate");
+  tc_core = tcase_create("core");
+
+  tcase_add_test(tc_core, TEST_COORDINATE_SHOULD_BE_OKAY);
+
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
---
+++ tests/test_synchronization/test_coordinate_based_matrix_sum/test_coordinate_based_matrix_sum.h
@@ -0,0 +1,13 @@
+#include <check.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "../../../src/synchronization/coordinate_based_matrix_sum/coordinate.h"
+
+Suite *coordinate_suite(void);
+
+START_TEST(TEST_COORDINATE_SHOULD_BE_OKAY)
+{
+    coordinate_based_matrix_sum();
+}
+END_TEST
