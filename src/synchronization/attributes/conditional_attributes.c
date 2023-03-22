574345d356e03b9d04616b3bc0278d11bbae1506
xyderos <kostas@xyderos.com>
2023-03-22 13:08:18 +0100

    added conditional atrributes for initialization

---
+++ src/synchronization/attributes/conditional_attributes.c
@@ -0,0 +1,20 @@
+#include <bits/pthreadtypes.h>
+#include <pthread.h>
+
+#include "conditional_attributes.h"
+
+void
+conditional_attributes(void)
+{
+	clockid_t clock_id;
+	pthread_condattr_t attrs;
+
+	// sets whether or not this conditional variable shall be shareable
+	// between other processes
+	pthread_condattr_setpshared(&attrs, 1);
+
+	// sets the clocked attribute for timedwait
+	pthread_condattr_setclock(&attrs, clock_id);
+
+	pthread_condattr_init(&attrs);
+}
